67c731eff7c6e6ded2dc78e10bbcefb27af1888a
VicDesotelle1 <103271992+VicDesotelle1@users.noreply.github.com>
2024-03-22 16:08:46 -0500

    Add files via upload

---
+++ wp-admin/post.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=http://discoverycolabs.com/vicsblog/wp-login.php?redirect_to=http%3A%2F%2Fdiscoverycolabs.com%2Fvicsblog%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D547&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "http://discoverycolabs.com/vicsblog/wp-login.php?redirect_to=http%3A%2F%2Fdiscoverycolabs.com%2Fvicsblog%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D547&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="http://discoverycolabs.com/vicsblog/wp-login.php?redirect_to=http%3A%2F%2Fdiscoverycolabs.com%2Fvicsblog%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D547&reauth=1">http://discoverycolabs.com/vicsblog/wp-login.php?redirect_to=http%3A%2F%2Fdiscoverycolabs.com%2Fvicsblog%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D547&reauth=1</a></p>
+	</body>
+</html>
